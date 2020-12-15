1d17d90ef94cb28a360dea3c017414bbe14b7fe1
diozhn <contato.diogomendes@gmail.com>
2020-12-15 14:31:27 -0300

    Update front-end, add login modal and map tooltip

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ import initTabNav from './modules/tabnav.js';
 import initAccordeon from './modules/accordeon.js';
 import initScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initModal from './modules/modal.js'
+import initTooltip from './modules/tooltip.js'
 
 initTabNav();
 initAccordeon();
 initScrollSuave();
 initAnimacaoScroll();
+initModal();
+initTooltip();
