c3393a10143d109ee92223d471bd65ee6a9241e6
diozhn <contato.diogomendes@gmail.com>
2020-12-16 13:06:22 -0300

    Added dropdown menu for mobile and desktop

---
+++ modules/dropdown.js
@@ -0,0 +1,21 @@
+import outsideClick from './outsideclick.js'
+
+export default function initDropDown() {
+    
+const dropdownMenu = document.querySelectorAll("[data-dropdown]");
+
+dropdownMenu.forEach((menu) => {
+  ["touchstart", "click"].forEach((userEvent) => {
+    menu.addEventListener(userEvent, handleClick);
+  });
+});
+
+function handleClick(event) {
+  event.preventDefault();
+  this.classList.add("ativo");
+  outsideClick(this,['touchstart', 'click'], () => {
+    this.classList.remove("ativo");
+  });
+}
+}
+initDropDown();
--- script.js
+++ script.js
@@ -2,8 +2,9 @@ import initTabNav from './modules/tabnav.js';
 import initAccordeon from './modules/accordeon.js';
 import initScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
-import initModal from './modules/modal.js'
-import initTooltip from './modules/tooltip.js'
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropDown from './modules/dropdown.js';
 
 initTabNav();
 initAccordeon();
@@ -11,3 +12,4 @@ initScrollSuave();
 initAnimacaoScroll();
 initModal();
 initTooltip();
+initDropDown();
